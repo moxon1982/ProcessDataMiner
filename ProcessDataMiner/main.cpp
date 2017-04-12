f73c2b68b6b585b3f7342581e5b64eae60381bcb
moxon1982 <mi.kerber@hotmail.com>
2017-04-12 17:04:22 +0200

    Added HeapList
    Heaplist is another method to search the memory for information.

--- ProcessDataMiner/main.cpp
+++ ProcessDataMiner/main.cpp
@@ -1,9 +1,27 @@
 #include "processdataminer.h"
+#include <string>
+#include <ctime>
 
 using namespace std;
 
 int main(void) {
-    ProcessDataMiner PDM("Patrizier");
+    string seed = "Patrizier", chunk;
+    ProcessDataMiner PDM(seed);
+
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    cout << dt << endl;
+
+    PDM.chunk = "Stockholm";
+    PDM.getHeapInfo();
+
+    now = time(0);
+    dt = ctime(&now);
+    cout << dt << endl;
+
+
     //PDM.displayInfo();
+    //chunk = "Stockholm";
+    //PDM.mineForInfo(chunk);
 }
 
--- ProcessDataMiner/processdataminer.h
+++ ProcessDataMiner/processdataminer.h
@@ -1,72 +1,63 @@
 #ifndef PROCESSDATAMINER_H
 #define PROCESSDATAMINER_H
 
-// check if header Files have already being included before
 #include <string>
 #include <windows.h>
 #include <tlHelp32.h>
 #include <iostream>
 
-
 using namespace std;
 
 class ProcessDataMiner {
     public:
+        // Initialize the class
         ProcessDataMiner(void);                     // default constructor
-        ProcessDataMiner(string proc_name);         // constructor initialize with process name
-        BOOL findProcessId(void);                   // find the process with the text fragment <str_proc_name>
-        BOOL findModuleInfo(DWORD);                 // find more information about the process found with findProcessID()
+        ProcessDataMiner(string& proc_name);        // constructor initialize with process name
+        void initPDM(string& proc_name);            // if for what ever case the default constructor had been called the class must be initialized manually
+
+        // functions to work with
+        BOOL findProcessId(void);                   // find the process with the text fragment <str_procname>
+        BOOL getHeapInfo(void);                     // another variant to find information about the process
         void displayInfo(void);                     // for testing you can display information with this method
-        void initPDM(string proc_name);             // if for what ever case the default constructor had been called the class must be initialized manually
+
+        string chunk;                               // this might probably get an array and will be private -> will be replaced by a public method set to work with
+
+        // probably obsolete funktions
+        void mineForInfo(string& chunk);
+        BOOL findModuleInfo(DWORD);                 // find more information about the process found with findProcessID()
     private:
-        string str_proc_name;                       // text fragment the process being looked for contains
-        DWORD proc_id;                              // process id provided by findProcessID()
-        LPCVOID baseAddress;                        // base address of the found process provided by findModuleInfo(DWORD)
-        SIZE_T size;                                // base address range size measured in bytes
-        BOOL proc_found;                            // if a valid process has been found this variable will be TRUE
-        LPBYTE buffer;
-        SIZE_T numOfBytesRead;
+        string str_procname;                        // text fragment the process being looked for contains
+        DWORD dw_procid;                            // process id provided by findProcessID()
+        void mineHeapForInfo(DWORD&, DWORD&);
+
+        // these will probably become obsolete ->being replaced by the methods to read out the heap of the process
+        LPCVOID lpcv_baseaddress;                   // base address of the found process provided by findModuleInfo(DWORD)
+        SIZE_T sizet_modbasesize;                   // base address range size measured in bytes
+        BOOL bool_procfound;                        // if a valid process has been found this variable will be TRUE
+        LPBYTE lpb_buffer;                          // buffer to store read information into
+        SIZE_T sizet_numbytesread;                  // number of bytes read
 };
 
 // default constructor
 ProcessDataMiner::ProcessDataMiner(void) {
-    str_proc_name = "";
-    proc_found = FALSE;
+    str_procname = "";
+    bool_procfound = FALSE;
+    chunk = "";
 }
 
 // constructor with process name
-ProcessDataMiner::ProcessDataMiner(string proc_name) {
-    str_proc_name = proc_name;
-    proc_found = FALSE;
+ProcessDataMiner::ProcessDataMiner(string& proc_name) {
+    str_procname = proc_name;
+    bool_procfound = FALSE;
+    chunk = "";
 
     if(findProcessId()) {
-        if(!findModuleInfo(proc_id)) {
+        if(!findModuleInfo(dw_procid)) {
             cerr << "Something went wrong reading module info!" << endl;
         }
     }
     else {
-        cerr << "No Process with the fragment " << str_proc_name << " in its name could be found!" << endl;
-    }
-
-    buffer = new BYTE[size];
-    DWORD dwBlock, dwOffset;
-
-    BYTE byte;
-
-    string text="";
-
-    if(Toolhelp32ReadProcessMemory(proc_id, baseAddress, buffer, size, &numOfBytesRead) == TRUE) {
-        for(dwBlock = 0; dwBlock < size; dwBlock+=16) {
-            for(dwOffset = 0; dwOffset < 16; dwOffset++) {
-                byte =*(buffer+dwBlock+dwOffset);
-                if(32 <= byte && byte < 127) {
-                    text+=byte;
-                }
-            }
-            if(text.find("Stockholm") != string::npos)
-                cout << text << endl;
-            text="";
-        }
+        cerr << "No Process with the fragment " << str_procname << " in its name could be found!" << endl;
     }
 }
 
@@ -75,23 +66,25 @@ ProcessDataMiner::ProcessDataMiner(string proc_name) {
  * @param proc_name
  *
  * This method initialized the class in case the default constructor has been used.
- * If the default constructor has been called the element <str_proc_name> will be
+ * If the default constructor has been called the element <str_procname> will be
  * empty thus leading other methods to fail because needed information is missing.
  */
-void ProcessDataMiner::initPDM(string proc_name) {
-    str_proc_name = proc_name;
-    proc_found = FALSE;
+void ProcessDataMiner::initPDM(string& proc_name) {
+    str_procname = proc_name;
+    bool_procfound = FALSE;
+    chunk = "";
 
     if(findProcessId()) {
-        if(!findModuleInfo(proc_id)) {
+        if(!findModuleInfo(dw_procid)) {
             cerr << "Something went wrong reading module info!" << endl;
         }
     }
     else {
-        cerr << "No Process with the fragment " << str_proc_name << " in its name could be found!" << endl;
+        cerr << "No Process with the fragment " << str_procname << " in its name could be found!" << endl;
     }
 }
 
+// will need to be reworked if obsolete elemets will be removed
 /**
  * @brief ProcessDataMiner::displayInfo
  *
@@ -99,14 +92,14 @@ void ProcessDataMiner::initPDM(string proc_name) {
  * for debugging purposes.
  */
 void ProcessDataMiner::displayInfo(void) {
-    if(!proc_found) {
+    if(!bool_procfound) {
         cerr << "No Process could be found thus displyInfo(void) shows nothing!" << endl;
     }
     else {
-        cout << "Process Name = " << str_proc_name << endl;
-        cout << "Process ID   = " << proc_id << endl;
-        cout << "Base address = " << baseAddress << endl;
-        cout << "Base size    = " << size << endl;
+        cout << "Process Name = " << str_procname << endl;
+        cout << "Process ID   = " << dw_procid << endl;
+        cout << "Base address = " << lpcv_baseaddress << endl;
+        cout << "Base size    = " << sizet_modbasesize << endl;
     }
 }
 
@@ -115,22 +108,20 @@ void ProcessDataMiner::displayInfo(void) {
  * @brief ProcessDataMiner::findProcessId
  * @return BOOL
  *
- * This method trys to find the process that contains the text fragment <str_proc_name>.
+ * This method trys to find the process that contains the text fragment <str_procname>.
  * If it fails to find a process with that name the class initialization fails and you
  * cannot continue using the object.
  */
 BOOL ProcessDataMiner::findProcessId(void) {
-    HANDLE hProcessSnap;
-    HANDLE hProcess;
     PROCESSENTRY32 pe32;
-    DWORD dwPriorityClass;
+    HANDLE hProcessSnap;
 
     /* as we are trying to find the process again though we already found it once
-     * -> proc_found == TRUE -> we reset the variable so the initialization would
+     * -> bool_procfound == TRUE -> we reset the variable so the initialization would
      * crash unexpected
      */
-    if(proc_found) {
-        proc_found = FALSE;
+    if(bool_procfound) {
+        bool_procfound = FALSE;
     }
 
     // Take a snapshot of all processes in the system.
@@ -139,50 +130,174 @@ BOOL ProcessDataMiner::findProcessId(void) {
         cerr << "CreateToolhelp32Snapshot (of processes)" << endl;
         return( FALSE );
     }
+    else {
+        // Set the size of the structure before using it.
+        pe32.dwSize = sizeof(PROCESSENTRY32);
 
-    // Set the size of the structure before using it.
-    pe32.dwSize = sizeof(PROCESSENTRY32);
+        // Retrieve information about the first process, and exit if unsuccessful
+        if(Process32First(hProcessSnap, &pe32) != FALSE) {
+            // Now walk the snapshot of processes to the the process <str_procname>
+            do {
+                // convert wchar_t into string
+                wchar_t* txt = pe32.szExeFile;
+                wstring ws(txt);
+                string str(ws.begin(),ws.end());
 
-    // Retrieve information about the first process, and exit if unsuccessful
-    if(!Process32First(hProcessSnap, &pe32)) {
-        cerr << "Process32First" << endl;
-        CloseHandle(hProcessSnap);          // clean the snapshot object
+                if (str.find(str_procname) != string::npos) {
+                    dw_procid = pe32.th32ProcessID;
+                    CloseHandle(hProcessSnap);
+                    return(TRUE);
+                }
+            } while(Process32Next(hProcessSnap, &pe32));
+        }
+        else {
+            cerr << "Process32First" << endl;
+            CloseHandle(hProcessSnap);          // clean the snapshot object
+            return(FALSE);
+        }
+        // nothing found. close handle and return false
+        CloseHandle(hProcessSnap);
+        return(FALSE);
+    }
+}
+
+/**
+ * @brief ProcessDataMiner::getHeapInfo
+ * @return BOOL
+ *
+ * This method creats a heaplist of the process id provided which it walks through calling the
+ * method mineHeapForInfo(DWORD&, DWORD&) which it gives the heap address and size to look for.
+ */
+BOOL ProcessDataMiner::getHeapInfo(void) {
+    HANDLE hHeapSnap;
+    HEAPLIST32 hl32 = {0};
+    HEAPENTRY32 he32 = {0};
+
+    cout << chunk << endl;
+
+    // Take a snapshot of all modules in the specified process.
+    hHeapSnap = CreateToolhelp32Snapshot(TH32CS_SNAPHEAPLIST, dw_procid);
+
+    // if the handle is invalid return false
+    if(hHeapSnap != INVALID_HANDLE_VALUE) {
+        // Set the size of the structure before using it.
+        hl32.dwSize = sizeof(HEAPLIST32);
+
+        // Retrieve information about the first module, and exit if unsuccessful
+        if(Heap32ListFirst(hHeapSnap, &hl32) != FALSE) {
+            // Set the size of the structure before using it.
+            he32.dwSize = sizeof(HEAPENTRY32);
+
+            // Now walk the heap list of the process
+            do {
+                if(Heap32First(&he32, hl32.th32ProcessID, hl32.th32HeapID) != FALSE) {
+                    do {
+                        mineHeapForInfo(he32.dwAddress, he32.dwBlockSize);
+                    } while(Heap32Next(&he32) != FALSE);
+                }
+                else { break; }
+            } while(Heap32ListNext(hHeapSnap, &hl32) != FALSE);
+        }
+        else {
+            cerr << "HEAP32ListFirst" << endl;
+            CloseHandle(hHeapSnap);           // clean the snapshot object
+            return(FALSE);
+        }
+    }
+    else {
+        cerr << "CreateToolhelp32Snapshot (of the heaplist)" << endl;
         return(FALSE);
     }
 
-    // Now walk the snapshot of processes to the the process <str_proc_name>
-    do {
-        // convert wchar_t into string
-        wchar_t* txt = pe32.szExeFile;
-        wstring ws(txt);
-        string str(ws.begin(),ws.end());
-
-        if (str.find(str_proc_name) != string::npos) {
-            // Retrieve the priority class.
-            dwPriorityClass = 0;
-            hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
-
-            if(hProcess == NULL) {
-                cerr << "printError: OpenProcess" << endl;
-            }
-            else {
-                dwPriorityClass = GetPriorityClass(hProcess);
-                if(!dwPriorityClass) {
-                    cerr << "printError: GetPriorityClass" << endl;
-                }
-                CloseHandle(hProcess);
-            }
-
-            // forward process id to class element
-            proc_id = pe32.th32ProcessID;
-            CloseHandle(hProcessSnap);
-            return(TRUE);
-        }
-    } while(Process32Next(hProcessSnap, &pe32));
-
-    // nothing found. close handle and return false
-    CloseHandle(hProcessSnap);
+    CloseHandle(hHeapSnap);
     return(FALSE);
+}
+
+/**
+ * @brief ProcessDataMiner::mineHeapForInfo
+ * @param dw_address
+ * @param dw_blocksize
+ *
+ * Provided the heap address and size it will search this resulting string for a chunk of text
+ * and outputs the address of the heap where the chunk had been seen in.
+ */
+void ProcessDataMiner::mineHeapForInfo(DWORD& dw_address, DWORD& dw_blocksize) {
+    LPBYTE pBuffer;
+    BYTE byte;
+    DWORD dwBytesRead, dwBlock, dwOffset;
+    string text="";
+
+    // initialize the pBuffer with the size of the current heap
+    pBuffer = new BYTE[dw_blocksize];
+
+    // read the information from the heap at dw_address with the size of dw_blocksize
+    if(Toolhelp32ReadProcessMemory(dw_procid, (LPCVOID) dw_address, pBuffer, dw_blocksize, &dwBytesRead) == TRUE) {
+        // read the whole heap so strings can not be cut off
+        // this loop might get obsolete in future if it turns out to be useful
+        for(dwBlock=0; dwBlock < dw_blocksize; dwBlock += dw_blocksize) {
+            // cout << hex << dw_address + dwBlock << ";";
+            for(dwOffset=0; dwOffset < dw_blocksize; dwOffset++) {
+                // get the byte at the address *(pBuffer + dwBlock + dwOffset)
+                byte = *(pBuffer + dwBlock + dwOffset);
+
+                // check if printable characters
+                if(32 <= byte && byte < 127) {
+                    text += byte;
+                }
+                // if not place a '.'-character
+                else {
+                    text += ".";
+                }
+            }
+
+            // cout << text << endl;
+
+            // check if chunk could be found and output the address of the heap the chunk had been found in and the whole string of the heap
+            if(text.find(chunk) != string::npos) {
+                cout << "HeapAddress: " << hex << dw_address;
+                cout << " // HeapString: " << text << endl;
+            }
+            // reset text
+            text="";
+        }
+    }
+}
+/*****************************************************************************************************************************************************/
+
+
+
+// might get obsolete
+void ProcessDataMiner::mineForInfo(string& chunk) {
+    DWORD dwBlock, dwOffset;
+    BYTE byte;
+    string text="";
+
+    lpb_buffer = new BYTE[sizet_modbasesize];
+
+    if(Toolhelp32ReadProcessMemory(dw_procid, lpcv_baseaddress, lpb_buffer, sizet_modbasesize, &sizet_numbytesread) == TRUE) {
+        cout << "NumBytesRead: " << sizet_numbytesread << endl;
+        for(dwBlock = 0; dwBlock < sizet_modbasesize; dwBlock+=16) {
+            for(dwOffset = 0; dwOffset < 16; dwOffset++) {
+                byte =*(lpb_buffer+dwBlock+dwOffset);
+                if(32 <= byte && byte < 127) {
+                    text+=byte;
+                }
+                else if(byte == '\0') {
+                    text+="0";
+                }
+                else {
+                    text+=".";
+                }
+            }
+            if(text.find(chunk) != string::npos) {
+
+                cout << "Address: " << hex << *(lpb_buffer + dwBlock);
+                cout << " // String: " << text << endl;
+            }
+            // reset text
+            text="";
+        }
+    }
 }
 
 /**
@@ -199,10 +314,10 @@ BOOL ProcessDataMiner::findModuleInfo(DWORD dwPID) {
     MODULEENTRY32 me32;
 
     /* as we are trying to find the process information again though we already found some once
-     * -> proc_found == TRUE -> we reset the variable so the initialization would crash unexpected
+     * -> bool_procfound == TRUE -> we reset the variable so the initialization would crash unexpected
      */
-    if(proc_found) {
-        proc_found = FALSE;
+    if(bool_procfound) {
+        bool_procfound = FALSE;
     }
 
     // Take a snapshot of all modules in the specified process.
@@ -224,17 +339,17 @@ BOOL ProcessDataMiner::findModuleInfo(DWORD dwPID) {
         return(FALSE);
     }
 
-    // Now walk the module list of the process to find the module <str_proc_name>
+    // Now walk the module list of the process to find the module <str_procname>
     do {
         // convert wchar_t into string
         wchar_t* txt = me32.szExePath;
         wstring ws(txt);
         string exe_path(ws.begin(),ws.end());
 
-        if (exe_path.find(str_proc_name) != string::npos) {
-            baseAddress = me32.modBaseAddr;
-            size = me32.modBaseSize;
-            proc_found = TRUE;
+        if (exe_path.find(str_procname) != string::npos) {
+            lpcv_baseaddress = me32.modBaseAddr;
+            sizet_modbasesize = me32.modBaseSize;
+            bool_procfound = TRUE;
             CloseHandle(hModuleSnap);
             return(TRUE);
         }
@@ -243,4 +358,6 @@ BOOL ProcessDataMiner::findModuleInfo(DWORD dwPID) {
     CloseHandle(hModuleSnap);
     return(FALSE);
 }
+
+
 #endif // PROCESSDATAMINER_H
